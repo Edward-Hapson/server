6e753a39537a7a2442129ff70a0ed09ce17276e6
Edward-Hapson <edwardhapson@gmail.com>
2023-11-12 15:59:51 +0300

    adding routes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
+const router = require('./routes/main')
 const app = express();
 
 
-app.get('/api',(req,res)=>{
-   res.json('hello, world') 
-})
+app.use('/api', router)
+// app.get('/api',(req,res)=>{
+//    res.json('hello, world') 
+// })
 
 app.listen(4000, ()=> {
     console.log('server connected on port 4000')
